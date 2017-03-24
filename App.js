df524d783bb5a6a8069ddd00d57af29d3c3c5618
bernulander <bernulander@gmail.com>
2017-03-24 13:55:58 -0700

    finished timer

--- App.js
+++ App.js
@@ -7,15 +7,53 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { clockDisplay: '00:00:00:00' }
+    this.state = { clockDisplay: '00:00:00:00',
+                    elapsedTime: 0,
+                    startTime: null };
+    this.startStopTimer = this.startStopTimer.bind(this);
+    // this.currentElapsedTime = this.currentElapsedTime.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
 
-  startStopTimer(){
-    console.log('start/stop timer');
+  startStopTimer() {
+
+    if(this.state.startTime) {
+      clearInterval(this.timer);
+      const newTime = this.state.elapsedTime + (new Date() - this.state.startTime);
+      this.setState({ startTime: null, elapsedTime: newTime});
+    } else {
+      this.setState({ startTime: new Date() });
+      this.timer = setInterval( () => {
+        const newTime = this.state.elapsedTime + (new Date() - this.state.startTime);
+        this.setState({ clockDisplay: this.convertMsToDigitalClock(newTime) });
+      }, 20);
+    }
+  }
+
+  // currentElapsedTime() {
+  //   this.state.elapsedTime + (new Date() - this.state.startTime);
+  // }
+
+  convertMsToDigitalClock(ms) {
+    const hours = Math.floor(ms / 3600000); // 1 Hour = 36000 Milliseconds
+    const minutes = Math.floor((ms % 3600000) / 60000); // 1 Minutes = 60000 Milliseconds
+    const seconds = Math.floor(((ms % 360000) % 60000) / 1000); // 1 Second = 1000 Milliseconds
+    const remainMs = Math.floor((ms - 3600000 * hours - minutes * 60000 - seconds * 1000) / 10);
+    return `${this.twoDigits(hours)}:${this.twoDigits(minutes)}:${this.twoDigits(seconds)}:${this.twoDigits(remainMs)}`;
+  }
+  twoDigits(number) {
+    if(number >= 10) {
+      return number.toString();
+    } else {
+      return `0${number.toString()}`
+    }
   }
 
   resetTimer() {
-    console.log('reset timer');
+    clearInterval(this.timer);
+    this.setState({ clockDisplay: '00:00:00:00',
+                   elapsedTime: 0,
+                   startTime: null });
   }
 
   render() {
@@ -29,7 +67,7 @@ export default class App extends React.Component {
           <TouchableHighlight underlayColor='#ff663d'
                               onPress={this.startStopTimer}
                               style={styles.startButton} >
-            <Text>Start</Text>
+            <Text>{this.state.startTime ? 'Stop' : 'Start'}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight underlayColor='#ff663d'
